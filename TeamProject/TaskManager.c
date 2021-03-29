b4b868806ea1c61b2eec456f491473a05687f981
JonathanDenoon <63335024+JonathanDenoon@users.noreply.github.com>
2021-03-28 23:33:45 -0400

    Add files via upload

---
+++ TeamProject/TaskManager.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "Manager.h"
+
+
+
+int main()
+{
+	int i;//loop counter variable
+	//Initialize an array of structs to hold the task list
+	struct TaskManager list[10];
+	for (i=0; i<10; i++)
+	{
+		list[i] = initialize();
+	}
+
+	//used to allow the while loop to run until a condition is met
+	bool complete = false;
+
+	//loops while the complete condition is not met. Once an invalid input
+	//is detected, then the complete condition is met and the module will return
+	//to the main program
+	while (!complete) 
+	{
+		char selection = ' ';
+
+		//input selection prompt to navigate this module
+		printf("Choose one of the following options from the menu below\n");
+		printf("To add a new task or update an existing one - (a)\n");
+		printf("To delete an existing task - (b)\n");
+		printf("To display current task list - (c)\n");
+		printf("To print all tasks in list - (d)\n");
+		printf("Your choice:");
+	
+		scanf("%c",&selection);
+
+
+		//Calls various functions depending on the input
+		switch (selection)
+		{
+		case 'a':
+			addNewUpdate(list);
+			break;
+
+		case 'b':
+			removeFromList(list);
+			break;
+
+		case 'c':
+			displayTask(list);
+			break;
+
+		case 'd':
+			displayAll(list);
+			break;
+
+		default:
+			printf("Returning to Main Menu...\n");
+			complete = true;
+		}
+	
+	}
+
+
+	return (0);
+}
