ee98584fa05df9fd45b48d68f36bb30cfa6ff46b
Jonathan Denoon <Jdenoon5901@conestogac.on.ca>
2021-04-14 21:54:21 -0400

    Made space for save/load code

--- TeamProject/TaskManager.c
+++ TeamProject/TaskManager.c
@@ -200,12 +200,12 @@ int taskMenu()
 			break;
 		case'g':
 			
-			printf("Still implementing, please come back later\n");
+
 			break;
 
 		case'h':
 			
-			printf("Still implementing, please come back later\n");
+
 			break;
 
 		case 'q':
